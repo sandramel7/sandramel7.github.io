8adae3314e35fdfc9f3cf34336e74323cb6e1323
sandramel7 <sashulka257@gmail.com>
2024-12-15 01:19:33 +0200

    v001

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   splitTextIntoSpans("h2" );
 
-  document.querySelectorAll("h2).forEach(h2 => {
+  document.querySelectorAll("h2").forEach(h2 =>{
     gsap.fromTo(h2.querySelectorAll("span"), {
       x: '-100%',
       filter: 'blur(8px)',
@@ -39,6 +39,33 @@ document.addEventListener('DOMContentLoaded', function () {
       stagger: 0.05,
       scrollTrigger: {
         trigger: h2,
+        start: "top 90%", // Adjust if necessary
+        end: "top 20%", // Ensures animation completes
+        toggleActions: "restart none none reset",
+        scrub: 1,
+        markers: false
+      },
+    });
+  });
+});
+
+
+      splitTextIntoSpans("p" );
+
+  document.querySelectorAll("p").forEach(p =>{
+    gsap.fromTo(h2.querySelectorAll("span"), {
+      x: '-100%',
+      filter: 'blur(8px)',
+      opacity: 0,
+    }, {
+      x: '0%',
+      filter: 'blur(0)',
+      opacity: 1,
+      duration: 1.5,
+      ease: "power4.out",
+      stagger: 0.05,
+      scrollTrigger: {
+        trigger: p,
         start: "top 90%", // Adjust if necessary
         end: "top 20%", // Ensures animation completes
         toggleActions: "restart none none reset",
